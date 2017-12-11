a69d0ccbada68306bf372947dc1b7eab141f7f7f
Jeremy Hudspeth <jeremyhudspeth87@gmail.com>
2017-12-10 22:03:09 -0600

    Use of XML Layout

---
+++ app/code/local/ChicagoCubs/Roster/Block/Footer.php
@@ -0,0 +1,9 @@
+<?php
+class ChicagoCubs_Roster_Block_Footer
+    extends Mage_Core_Block_Template
+{
+    public function setRoster()
+    {
+        echo "The Chicago Cubs Are the Best";
+    }
+}S
