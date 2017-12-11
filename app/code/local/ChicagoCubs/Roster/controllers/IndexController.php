e7973817ab2b828c209e54b75e0427a3894ccb3a
Jeremy Hudspeth <jeremyhudspeth87@gmail.com>
2017-12-10 21:13:48 -0600

    Use of Controllers Beginning Cubs Roster Project)

---
+++ app/code/local/ChicagoCubs/Roster/controllers/IndexController.php
@@ -0,0 +1,6 @@
+<?php
+class ChicagoCubs_Roster_IndexController extends Mage_Core_Controller_Front_Action{
+    public function indexAction(){
+        echo "The Chicago Cubs Are the Best!!";
+    }
+}
