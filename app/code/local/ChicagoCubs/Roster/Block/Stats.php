e6e50061d8c9f1cd7d1631e95ff7a34e4a5b2e40
Jeremy Hudspeth <jeremyhudspeth87@gmail.com>
2018-01-26 00:31:45 -0600

    Model Updated2

--- app/code/local/ChicagoCubs/Roster/Block/Stats.php
+++ app/code/local/ChicagoCubs/Roster/Block/Stats.php
@@ -1,9 +1,29 @@
 <?php
-class ChicagoCubs_Roster_Block_Stats
-    extends Mage_Core_Block_Template
+
+class ChicagoCubs_Roster_Block_Stats extends Mage_Core_Block_Template
 {
-    public function setStats()
-    {
-        echo "Go Cubs GO";
-    }
-}
+     public function methodblock()
+     {
+
+        $retour='';
+
+        $collection = Mage::getModel('chicagocubs_roster/players')
+                            ->getCollection()
+                            ->setOrder('id_chicagocubs_roster','asc');
+
+
+
+        foreach($collection as $data)
+        {
+
+             $retour .= $data->getData('Name').'<br />';
+
+         }
+
+
+
+         Mage::getSingleton('adminhtml/session')->addSuccess('World Champions');
+
+         return $retour;
+      }
+ }
